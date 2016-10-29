5ced4a876057384fb9689d65f2b2a9b44dfb0c2e
dorayo <yomingyo@gmail.com>
2016-10-29 14:59:26 +0800

    Initial Commit

---
+++ ZHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHW
+//
+//  Created by 闫世超 on 16/10/29.
+//  Copyright © 2016年 闫世超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHW
+//
+//  Created by 闫世超 on 16/10/29.
+//  Copyright © 2016年 闫世超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
